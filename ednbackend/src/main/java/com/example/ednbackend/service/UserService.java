5369a82447181f09be0512d95eb19b8fdb05f130
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2025-01-29 22:52:42 +0000

    Login functionality

--- ednbackend/src/main/java/com/example/ednbackend/service/UserService.java
+++ ednbackend/src/main/java/com/example/ednbackend/service/UserService.java
@@ -1,7 +1,7 @@
-package com.example.demo.service;
+package com.example.ednbackend.service;
 
-import com.example.demo.models.User;
-import com.example.demo.repository.UserRepository;
+import com.example.ednbackend.models.User;
+import com.example.ednbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
