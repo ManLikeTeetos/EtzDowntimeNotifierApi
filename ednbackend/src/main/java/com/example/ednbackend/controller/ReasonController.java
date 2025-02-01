e0bad01435e3911a28b87b0de2f46c6a50d6661e
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2025-02-01 22:30:22 +0000

    Bank, Status and Reason entry api

--- ednbackend/src/main/java/com/example/ednbackend/controller/ReasonController.java
+++ ednbackend/src/main/java/com/example/ednbackend/controller/ReasonController.java
@@ -5,6 +5,7 @@ import com.example.ednbackend.repository.ReasonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.ednbackend.dto.ResponseMessage;  
 
 import java.util.List;
 
